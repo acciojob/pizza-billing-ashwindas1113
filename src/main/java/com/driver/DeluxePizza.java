5d7ecca121564639210d6700bb8898dc98aab010
Ashwin Das <ashwindas1113@gmail.com>
2023-09-16 13:04:55 -0500

    Assignment First Attempt

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -2,8 +2,10 @@ package com.driver;
 
 public class DeluxePizza extends Pizza {
 
-    public DeluxePizza(Boolean isVeg) {
+        public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+            addExtraCheese();
+            addExtraToppings();
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,25 +6,65 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private boolean extraCheeseAdded;
+    private boolean extraToppingsAdded;
+    private boolean paperBagAdded;
+
+
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+       if(isVeg){
+           price = 300;
+           bill = Integer.toString(price);
+       }else{
+           price = 400;
+           bill  = Integer.toString(price);
+       }
     }
 
     public int getPrice(){
+
         return this.price;
     }
 
     public void addExtraCheese(){
         // your code goes here
+
+        if(!extraCheeseAdded){
+            this.price = 80 + price;
+            this.bill = Integer.toString(price);
+            extraCheeseAdded = true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+
+
+        if(!extraToppingsAdded){
+            if(isVeg){
+                this.price = 70 + price;
+                this.bill = Integer.toString(price);
+                extraToppingsAdded = true;
+            }else{
+                this.price = 120 + price;
+                this.bill = Integer.toString(price);
+                extraToppingsAdded = true;
+            }
+        }
+
     }
 
     public void addTakeaway(){
         // your code goes here
+
+        if(!paperBagAdded) {
+            this.price = 20 + price;
+            this.bill = Integer.toString(price);
+            paperBagAdded = true;
+        }
     }
 
     public String getBill(){
